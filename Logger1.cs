b7d134c511231fa1ec9e478e72e09d5dfe2efc66
jhon65496 <jhon65496@yandex.ru>
2024-03-26 00:03:51 +0300

    Добавьте файлы проекта.

---
+++ Logger1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+namespace SerilogTestWinForm01
+{
+    internal class Logger1
+    {
+        public void CreateLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                            // add console as logging target
+                            .WriteTo.Console(
+                                            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}") // 
+                                             
+                                                                                                                                          
+
+                            // set default minimum level
+                            // .Use
+                            .MinimumLevel.Debug()
+                            
+                            .CreateLogger();
+
+
+            string message = "`Parametr logging result`";
+
+            // logging                        
+            Log.Information($"Some `Information`. Parametr logging -- {message}");
+                        
+
+            Log.CloseAndFlush();
+        }
+
+
+    }
+}
